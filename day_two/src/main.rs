900ab91893cb8d4fc3daa08ede7f7e58adb59531
ivan <>
2022-12-09 00:15:23 +0100

    split parts in functinos

--- day_two/src/main.rs
+++ day_two/src/main.rs
@@ -7,12 +7,45 @@ fn main() {
     let game_list = File::open("gamelist.txt").expect("could not load file");
     let reader = BufReader::new(game_list);
 
+    //save all lines from gamelist.txt into Vec<string>
     let games: Vec<_> = reader
         .lines()
         .map(|line| line.unwrap())
         .filter(|characters| characters.starts_with("A") || characters.starts_with("B") || characters.starts_with("C"))
         .map(|split| split.split_whitespace().collect::<String>()).collect();
 
+    part_one(&games);
+    part_two(&games);
+
+}
+
+fn part_two(games: &Vec<String>) {
+
+    let mut total_score = 0;
+    let mut total_counter = 0;
+
+    for game in games.iter() {
+        if game.chars().nth(1).unwrap() == 'X' {
+            // println!("{}",game);
+            // total_score = total_score + 12;
+        } else if game.chars().nth(1).unwrap() == 'Y' {
+            // println!("{}",game);
+            // total_score = total_score + 15;
+        } else if game.chars().nth(1).unwrap() == 'Z' {
+            // println!("{}",game);
+            // total_score = total_score + 18;
+        } else {
+            println!("errror {}", game);
+        }
+        // println!("{}",game);
+        total_counter = total_counter + 1;
+    }
+    println!("#part_two total_score: {}", total_score);
+    println!("#part_two total_score: {}", total_counter);
+
+}
+
+fn part_one(games: &Vec<String>) {
     let mut total_score = 0;
     let mut total_counter = 0;
 
@@ -36,11 +69,12 @@ fn main() {
         } else if game == "CZ" {
             total_score = total_score + 6;
         } else {
-            println!("irgendebbes fehlt {}", game);
+            println!("error {}", game);
         }
         // println!("{}",game);
         total_counter = total_counter + 1;
     }
-    println!("total_score: {}", total_score);
-    println!("total_score: {}", total_counter);
+    println!("#part_one total_score: {}", total_score);
+    println!("#part_one total_score: {}", total_counter);
+
 }
