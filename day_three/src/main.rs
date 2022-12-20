055ed02a73700ac165df063bc492ca4e784fa107
Ivan <i.spina@live.com>
2022-12-20 18:18:18 +0100

    day_three part_one

--- day_three/src/main.rs
+++ day_three/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 fn main() {
     let rucksack_file = File::open("rucksack_list.txt").expect("error while opening file");
     let buff_reader = BufReader::new(rucksack_file);
+    let mut res_part_one: i32 = 0;
 
     for (rucksack_item_one, rucksack_item_two) in buff_reader
         .lines()
@@ -13,26 +14,27 @@ fn main() {
                 Err(_) => panic!("error while get string len"),
             };
             let (item_one, item_two) = match &rucksack_parts {
-                Ok(parts) => parts.split_at((len / 2)),
+                Ok(parts) => parts.split_at(len / 2),
                 Err(_) => panic!("error while splitting rucksack compartments"),
             };
-            if item_two.contains(item_one){
-
-            }
-            //FIXME return Vec<String>, Vec<String>
             (item_one.to_string(), item_two.to_string())
-            // println!("items {:?}",item_one);
-            // println!("items {:?}",item_two);
         })
     {
-        let mut found_char: Vec<char> = Vec::new();
         for item_a in rucksack_item_one.chars() {
             if rucksack_item_two.contains(item_a) {
-                println!("{}", item_a);
-                found_char.push(item_a);
+                    if item_a.is_lowercase(){
+                        res_part_one = res_part_one + item_a as i32 - 'a' as i32 + 1;
+                        //take the first element ¯\_(ツ)_/¯ ugly
+                        break;
+                    }else {
+                        res_part_one = res_part_one + item_a as i32 - 'A' as i32 + 27 ;
+                        //take the first element  ¯\_(ツ)_/¯ ugly
+                        break;
+                    }
             }
         }
-        //debug print
-        println!("{:?}", found_char.iter());
+    }
+    {
+            println!("part_one: {}",res_part_one);
     }
 }
